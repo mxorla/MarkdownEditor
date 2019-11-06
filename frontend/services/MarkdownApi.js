a3214e8776575f84626463999413fe013444f775
Matias <matias.orlandella@gmail.com>
2019-11-06 03:35:57 -0300

    remove service

--- frontend/services/MarkdownApi.js
+++
@@ -1,7 +0,0 @@
-import * as superagent from 'superagent';
-
-export default class MarkdownApi extends React.Component{
- async fetchDocumentEntries() {
-    return superagent.get(`http://localhost:5000/api/document`)
-  }
-}
